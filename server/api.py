ad77c5b4d82b26890652181bcae19e8bfd22bf77
Gemma Acevedo <41479572+wzcwzc05@users.noreply.github.com>
2024-03-03 23:18:20 +0800

    Update

--- server/api.py
+++ server/api.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, send_from_directory
 import json
 import os
 import versionManager as vM
@@ -15,21 +15,14 @@ required_keys = ["sha256", "version", "branch", "package", "local",
                  "remote", "BeforeUpdate", "AfterUpdate", "dependencies", "restore"]
 
 
-def check_content(content: dict) -> bool:
-    for i in required_keys:
-        if i not in content:
-            return False
-    return True
-
-
 @app.route("/test")
 def hello_world():
     dic = {"status": 200}
     return str(json.dumps(dic))
 
 
-@app.route("/maxVersion")
-def maxVersion() -> str:
+@app.route("/latestVersion")
+def maxVersion() -> str:    # 获取最新版本
     dic = {"status": 200, "content.json": {}}
     package = request.args.get("package")
     branch = request.args.get("branch")
@@ -46,12 +39,12 @@ def maxVersion() -> str:
 
 
 @app.route("/getVersion")
-def getVersion() -> str:
+def getVersion() -> str:    # 获取指定版本
     dic = {"status": 200}
     package = request.args.get("package")
     branch = request.args.get("branch")
     version = request.args.get("version")
-    if package is None or branch is None or version is None:
+    if package is None:
         dic["status"] = 400
         return str(json.dumps(dic))
     content = vM.getCVersion(package, branch, version)
@@ -59,24 +52,27 @@ def getVersion() -> str:
         dic["status"] = 404
         return str(json.dumps(dic))
     else:
-        dic["content.json"] = content
+        dic["list"] = content
         return str(json.dumps(dic))
 
 
-@app.route("/ota-file/<path_s>")
-def ota_file(path_s) -> bytes:
-    if not os.path.exists(os.path.join(storage_path, path_s)):
-        return str(404)
-    f = open(os.path.join(storage_path, path_s), "rb")
-    return f.read()
-
-
-@app.route('/update', methods=['PUT'])
-def update_file():
+@app.route('/ota-files/<path:filename>')    # 通过路由参数来获取文件名
+def ota_files(filename):
     dic = {"status": 200}
+    # 使用safe_join来防止路径遍历攻击
+    safe_path = os.path.join(storage_path, filename)
+    if not os.path.exists(safe_path):
+        dic["status"] = 404
+        return str(json.dumps(dic))
+    return send_from_directory(storage_path, filename)
+
+
+@app.route('/upload', methods=['PUT'])
+def upload_file():  # 上传文件
+    dic = {"status": 200}
+    isForce = request.args.get("force") == "1"
+
     # 检查是否有文件和JSON数据
-    print(request.form)
-    print(request.files)
     if 'file' not in request.files or 'content' not in request.form:
         dic["status"] = 400
         dic["error"] = "No file or content"
@@ -84,34 +80,51 @@ def update_file():
 
     file = request.files["file"]
     content = request.form['content']
-
+    content_data = {}
     try:
         content_data = json.loads(content)
-        if (check_content(content_data) == False):
+        if (vM.checkContent(content_data) == False):
             dic["status"] = 400
             dic["error"] = "Json Content Error"
             return str(json.dumps(dic))
-        expected_sha256 = content_data.get('sha256')
+        expected_sha256 = content_data['sha256']
     except json.JSONDecodeError:
         dic["status"] = 400
         dic["error"] = "JSONDecodeError"
         return str(json.dumps(dic))
 
+    name = content_data["package"]
+    branch = content_data["branch"]
+    version = content_data["version"]
+    file_name = name+"-"+branch+"-"+version+".zip"
+    file_path = os.path.join(storage_path, name, branch, file_name)
     file_content = file.read()
     sha256_hash = hashlib.sha256(file_content).hexdigest()
+
+    if os.path.exists(file_path) and not isForce:
+        dic["status"] = 409
+        dic["error"] = "File exists"
+        dic["TIP"] = "If you want to force update, please add a query parameter 'force=1' to the URL"
+        return str(json.dumps(dic))
+    if vM.checkVersion(name, branch, version) == True and not isForce:
+        dic["status"] = 409
+        dic["error"] = "Version exists"
+        dic["TIP"] = "If you want to force update, please add a query parameter 'force=1' to the URL"
+        return str(json.dumps(dic))
 
     if sha256_hash != expected_sha256:
         dic["status"] = 400
         dic["error"] = "SHA256 Error"
         return str(json.dumps(dic))
     try:
-        with open(os.path.join(storage_path, sha256_hash), "wb") as f:
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "wb") as f:
             f.write(file_content)
     except:
         dic["status"] = 500
         dic["error"] = "Write file Error"
         return str(json.dumps(dic))
-        
+
     try:
         if vM.writeinVersion(content_data) == False:
             dic["status"] = 409
@@ -121,7 +134,7 @@ def update_file():
         dic["status"] = 500
         dic["error"] = "Write database Error"
         return str(json.dumps(dic))
-    
+
     return str(json.dumps(dic))
 
 
--- server/versionManager.py
+++ server/versionManager.py
@@ -28,21 +28,31 @@ def getMaxVersion(package: str, branch: str) -> dict:   # 获取最新版本的�
         return results[0][0]
 
 
-def getCVersion(package: str, branch: str, version: str) -> dict:   # 获取特定版本的信息
+def getCVersion(package: str, branch=None, version=None) -> list:
+    # 建立数据库连接
     db = pymysql.connect(host=db_host,
                          user=db_user,
                          password=db_password,
                          database=db_database,
                          port=db_port)
-    cursor = db.cursor()
-    cursor.execute("SELECT content FROM ota WHERE name='%s' AND branch = '%s' AND version = '%s'" % (
-        package, branch, version))
-    results = cursor.fetchall()
-    db.close()
-    if (len(results) == 0):
-        return None
-    else:
-        return results[0][0]
+    try:
+        cursor = db.cursor(pymysql.cursors.DictCursor)
+        # 基础查询语句
+        sql = "SELECT name, branch, version, content FROM ota WHERE name=%s"
+        params = [package]  # 查询参数列表
+
+        if branch:
+            sql += " AND branch=%s"
+            params.append(branch)
+        if version:
+            sql += " AND version=%s"
+            params.append(version)
+
+        cursor.execute(sql, params)
+        results = cursor.fetchall()
+        return results
+    finally:
+        db.close()
 
 
 def writeinVersion(content: dict) -> bool:  # 写入版本信息
@@ -52,14 +62,40 @@ def writeinVersion(content: dict) -> bool:  # 写入版本信息
                          database=db_database,
                          port=db_port)
     cursor = db.cursor()
+    sql = ""
+    if (checkVersion(content["package"], content["branch"], content["version"])):
+        sql = "UPDATE ota SET content='%s' WHERE name='%s' AND branch='%s' AND version='%s'" % (
+            json.dumps(content), content["package"], content["branch"], content["version"])
+    else:
+        sql = "INSERT INTO ota (name, version, branch, content) VALUES ('%s', '%s', '%s', '%s')" % (
+            content["package"], content["version"], content["branch"], json.dumps(content))
+    cursor.execute(sql)
+    db.commit()
+    db.close()
+    return True
+
+
+def checkContent(content: dict) -> bool:
+    required_keys = ["sha256", "version", "branch", "package", "local",
+                     "remote", "BeforeUpdate", "AfterUpdate", "dependencies", "restore"]
+    for i in required_keys:
+        if i not in content:
+            return False
+    return True
+
+
+def checkVersion(package: str, branch: str, version: str) -> bool:
+    db = pymysql.connect(host=db_host,
+                         user=db_user,
+                         password=db_password,
+                         database=db_database,
+                         port=db_port)
+    cursor = db.cursor()
     cursor.execute("SELECT * FROM ota WHERE name='%s' AND branch = '%s' AND version = '%s'" % (
-        content["package"], content["branch"], content["version"]))
+        package, branch, version))
     results = cursor.fetchall()
-    if (len(results) != 0):
+    if (len(results) == 0):
         db.close()
         return False
-    cursor.execute("INSERT INTO ota (name, version, branch, content) VALUES ('%s', '%s', '%s', '%s')" % (
-        content["package"], content["version"], content["branch"], json.dumps(content)))
-    db.commit()
     db.close()
     return True
